bfb50ebc879819cf140507da0b10be6d9efc5fff
Tim N <57206024+NTimothy@users.noreply.github.com>
2022-07-28 13:19:26 -0500

    Update time_calculator.py

--- Scientific Computing with Python/time_calculator.py
+++ Scientific Computing with Python/time_calculator.py
@@ -1,1 +1,8 @@
-
+def add_time(start, duration, weekday=""):
+  _now = start.split(":")
+  print(_now)
+  current_time = {
+    "day": "",
+    "hour": int(_now[0]),
+    "minute": int(_now[1][:2]),
+  }
