5c05f93a246722c311355a0f04b1d11840895918
Tim N <57206024+NTimothy@users.noreply.github.com>
2022-08-03 23:43:36 -0500

    Create budget.py
    - Create initial framework for BankAccount class and function "create_spend_chart"

---
+++ Scientific Computing with Python/budget.py
@@ -0,0 +1,25 @@
+class BankAccount:
+    def __init__(self):
+        pass
+    
+    def __repr__(self):
+        pass
+    
+    def deposit(self):
+        pass
+    
+    def withdraw(self):
+        pass
+    
+    def get_balance(self):
+        pass
+    
+    def transfer(self):
+        pass
+    
+    def check_funds(self):
+        pass
+    
+def create_spend_chart(category1,category2,category3):
+    bar_chart = ""
+    return bar_chart
