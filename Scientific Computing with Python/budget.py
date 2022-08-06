65705db552c29f686e57af7c3de7ad2b0c9fa01e
Tim N <57206024+NTimothy@users.noreply.github.com>
2022-08-05 23:04:15 -0500

    Update budget.py

--- Scientific Computing with Python/budget.py
+++ Scientific Computing with Python/budget.py
@@ -9,16 +9,16 @@ class Category:
         body = "\n"
         for item in self.ledger:
             left_alignment = str(item["description"])[0:23]
-            right_alignment = str(format(float(item["amount"]),'.2f'))[0:7]
+            right_alignment = str(format(float(item["amount"]), '.2f'))[0:7]
             body += f"{left_alignment : <23}{right_alignment : >7}\n"
-        total_amt= format(float(self.balance), '.2f')
+        total_amt = format(float(self.balance), '.2f')
         total = f"Total: {total_amt}"
         display = str(header)+str(body)+total
         return display
 
     def deposit(self, amount, description=""):
         transaction = {}
-        transaction["amount"] = (amount)
+        transaction["amount"] = amount
         transaction["description"] = description
         self.ledger.append(transaction)
         self.balance += amount
@@ -27,7 +27,7 @@ class Category:
     def withdraw(self, amount, description=""):
         if self.check_funds(amount):
             transaction = {}
-            transaction["amount"] = -1 * (amount)
+            transaction["amount"] = -1 * amount
             transaction["description"] = description
             self.ledger.append(transaction)
             self.balance -= amount
@@ -48,7 +48,7 @@ class Category:
             successful_transfer = False
         return successful_transfer
 
-    def check_funds(self,amount):
+    def check_funds(self, amount):
         if amount > self.balance:
             available = False
         else:
@@ -56,42 +56,62 @@ class Category:
         return available
 
 
-def create_spend_chart(categories):
-    bar_chart = ""
-    return bar_chart
+def create_spend_chart(category_list):
+    data = {"Total": 0}
+    label_max = 0
+    for single_category in category_list:
+        # Determines and stores length of longest category name for labeling
+        length = len(single_category.name)
+        if length > label_max:
+            label_max = int(length)
 
+        # Create Key:Value pair of Category:Expenses
+        data[single_category.name] = 0
 
-def test():
-    food = Category("Food")
-    food.deposit(1000, "initial deposit")
-    food.withdraw(10.15, "groceries")
-    food.withdraw(15.89, "restaurant and more food for dessert")
-    print(food,"\n")
-    clothing = Category("Clothing")
-    food.transfer(50, clothing)
-    clothing.withdraw(25.55)
-    clothing.withdraw(100)
-    print(food)
-    print(clothing)
-    auto = Category("Auto")
-    auto.deposit(1000, "initial deposit")
-    auto.withdraw(15)
-    print(auto)
+        # Sum of withdrawals only per Category
+        for transaction in single_category.ledger:
+            if transaction["amount"]<0:
+                data[single_category.name] += abs(transaction["amount"])
+                data["Total"] += abs(transaction["amount"])
 
-    # print(food)
-    # print(clothing)
+    # Normalize withdrawals to % of total expenses
+    total = float(data["Total"])
+    for single_category in data:
+        data[single_category] = int((float(data[single_category])/total)*100)
 
+    # Delete unneeded key in preparation for parsing
+    data.pop("Total")
 
-def test_to_string():
-    food = Category("Food")
-    entertainment = Category("Entertainment")
-    food.deposit(900, "deposit")
-    food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-    food.transfer(20, entertainment)
-    print(food)
-    print("\nEXPECTED")
-    print(
-        f"*************Food*************\ndeposit                 900.00\nmilk, cereal, eggs, bac -45.67\nTransfer to Entertainme -20.00\nTotal: 834.33")
+    # Graph Title
+    title = "Percentage spent by category\n"
 
-# test()
-# test_to_string()
+    # Graph Data
+    graph = ""
+    num = 110
+    while num>0:
+        num -= 10
+        graph += f"{num : >3}| "
+        for single_category in data.keys():
+            if data[single_category] >= num:
+                graph += "o  "
+            else:
+                graph += "   "
+        graph += "\n"
+
+    # X-axis bar line
+    separator = "    -" + "---"*len(category_list)
+
+    char = 0
+    label = ""
+    while char < label_max:
+        label += "\n     "
+        for single_category in data.keys():
+            try:
+                if not single_category[char] == "":
+                    label += single_category[char] + "  "
+            except IndexError:
+                label += "   "
+        char += 1
+
+    chart = title + graph + separator + label
+    return chart
