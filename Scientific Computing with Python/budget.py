8ee44aa0d1fa7f917baf0cd1c16724d0ecd9afd4
Tim N <57206024+NTimothy@users.noreply.github.com>
2022-08-05 19:53:59 -0500

    Update budget.py
    - Updated Category class to be functional
    
    To be completed:
    - create_spend_chart method to draw visual representation as a string

--- Scientific Computing with Python/budget.py
+++ Scientific Computing with Python/budget.py
@@ -1,25 +1,51 @@
-class BankAccount:
-    def __init__(self):
-        pass
-    
+class Category:
+    def __init__(self, category_name="Misc"):
+        self.ledger = []
+        self.balance = 0
+        self.name = str(category_name.lower().capitalize())
+
     def __repr__(self):
-        pass
-    
-    def deposit(self):
-        pass
-    
-    def withdraw(self):
-        pass
-    
+        return self.name
+
+    def deposit(self, amount, description=""):
+        transaction = {}
+        transaction["amount"] = (amount)
+        transaction["description"] = description
+        self.ledger.append(transaction)
+        self.balance += amount
+        return True
+
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount):
+            transaction = {}
+            transaction["amount"] = -1 * (amount)
+            transaction["description"] = description
+            self.ledger.append(transaction)
+            self.balance -= amount
+            completed = True
+        else:
+            completed = False
+        return completed
+
     def get_balance(self):
-        pass
-    
-    def transfer(self):
-        pass
-    
-    def check_funds(self):
-        pass
-    
-def create_spend_chart(category1,category2,category3):
+        return self.balance
+
+    def transfer(self, amount, destination_category):
+        if self.check_funds(amount):
+            successful_transfer = True
+            self.withdraw(amount, f"Transfer to {destination_category}")
+            destination_category.deposit(amount, f"Transfer from {self}")
+        else:
+            successful_transfer = False
+        return successful_transfer
+
+    def check_funds(self,amount):
+        if amount > self.balance:
+            available = False
+        else:
+            available = True
+        return available
+
+def create_spend_chart(categories):
     bar_chart = ""
     return bar_chart
