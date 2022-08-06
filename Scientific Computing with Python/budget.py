3d546952e05d1357fae1a10cdde6cc0ab19cca7a
Tim N <57206024+NTimothy@users.noreply.github.com>
2022-08-05 20:50:58 -0500

    Update budget.py
    - Update  Category __repr__ dunder method to display ledger in readable format
    - Added test methods for convenience
    
    Final method be completed
    - create_spend_chart

--- Scientific Computing with Python/budget.py
+++ Scientific Computing with Python/budget.py
@@ -1,11 +1,20 @@
 class Category:
-    def __init__(self, category_name="Misc"):
+    def __init__(self, category_name=""):
         self.ledger = []
-        self.balance = 0
+        self.balance = float(0.0)
         self.name = str(category_name.lower().capitalize())
 
     def __repr__(self):
-        return self.name
+        header = self.name.center(30, "*")
+        body = "\n"
+        for item in self.ledger:
+            left_alignment = str(item["description"])[0:23]
+            right_alignment = str(format(float(item["amount"]),'.2f'))[0:7]
+            body += f"{left_alignment : <23}{right_alignment : >7}\n"
+        total_amt= format(float(self.balance), '.2f')
+        total = f"Total: {total_amt}"
+        display = str(header)+str(body)+total
+        return display
 
     def deposit(self, amount, description=""):
         transaction = {}
@@ -33,8 +42,8 @@ class Category:
     def transfer(self, amount, destination_category):
         if self.check_funds(amount):
             successful_transfer = True
-            self.withdraw(amount, f"Transfer to {destination_category}")
-            destination_category.deposit(amount, f"Transfer from {self}")
+            self.withdraw(amount, f"Transfer to {destination_category.name}")
+            destination_category.deposit(amount, f"Transfer from {self.name}")
         else:
             successful_transfer = False
         return successful_transfer
@@ -46,6 +55,43 @@ class Category:
             available = True
         return available
 
+
 def create_spend_chart(categories):
     bar_chart = ""
     return bar_chart
+
+
+def test():
+    food = Category("Food")
+    food.deposit(1000, "initial deposit")
+    food.withdraw(10.15, "groceries")
+    food.withdraw(15.89, "restaurant and more food for dessert")
+    print(food,"\n")
+    clothing = Category("Clothing")
+    food.transfer(50, clothing)
+    clothing.withdraw(25.55)
+    clothing.withdraw(100)
+    print(food)
+    print(clothing)
+    auto = Category("Auto")
+    auto.deposit(1000, "initial deposit")
+    auto.withdraw(15)
+    print(auto)
+
+    # print(food)
+    # print(clothing)
+
+
+def test_to_string():
+    food = Category("Food")
+    entertainment = Category("Entertainment")
+    food.deposit(900, "deposit")
+    food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+    food.transfer(20, entertainment)
+    print(food)
+    print("\nEXPECTED")
+    print(
+        f"*************Food*************\ndeposit                 900.00\nmilk, cereal, eggs, bac -45.67\nTransfer to Entertainme -20.00\nTotal: 834.33")
+
+# test()
+# test_to_string()
